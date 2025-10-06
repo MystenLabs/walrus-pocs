ae5ab63536e17b68f630ced65f89327fd32134c1
Kostas Tripalitakis <ktrip96@gmail.com>
2025-10-06 21:24:11 +0300

    Allow epochs option in the HTTP

--- http-api/src/upload.ts
+++ http-api/src/upload.ts
@@ -1,26 +1,40 @@
-import "dotenv/config"
-import fs from "fs";
-import fetch from "node-fetch";
+import 'dotenv/config'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { parseArgs } from './helper'
 
-async function upload(filePath: string) {
-    const url = `${process.env.PUBLISHER}/v1/blobs`
+async function upload(filePath: string, epochs?: number) {
+	let url = `${process.env.PUBLISHER}/v1/blobs`
 
-    const fileStream = fs.createReadStream(filePath)
-    const response = await fetch(url, {
-        method: "PUT",
-        body: fileStream as any,
-        headers: {
-            "Content-Type": "application/octet-stream",
-        },
-    })
-    const data: any = await response.json()
-    const blobId = data["newlyCreated"]["blobObject"]["blobId"]
-    return `
+	if (epochs !== undefined) {
+		url = `${url}?epochs=${epochs}`
+	}
+
+	const fileStream = fs.createReadStream(filePath)
+	const response = await fetch(url, {
+		method: 'PUT',
+		body: fileStream as any,
+		headers: {
+			'Content-Type': 'application/octet-stream',
+		},
+	})
+
+	if (!response.ok) {
+		const errorText = await response.text()
+		throw new Error(`Upload failed with status ${response.status}: ${errorText}`)
+	}
+
+	const data: any = await response.json()
+	const blobId = data['newlyCreated']['blobObject']['blobId']
+	return `
     Your file has been uploaded on Walrus!
     The blob id is: ${blobId}
+    Walrus Scan: https://walruscan.com/testnet/blob/${blobId}
     You can view it here: ${process.env.AGGREGATOR}/v1/blobs/${blobId}
     `
 }
 
-const args = process.argv.slice(2)
-upload(args[0]).then(console.log).catch(console.error)
+const rawArgs = process.argv.slice(2)
+const { content, epochs } = parseArgs(rawArgs)
+
+upload(content, epochs).then(console.log).catch(console.error)
--- http-api/src/write.ts
+++ http-api/src/write.ts
@@ -1,21 +1,36 @@
-import "dotenv/config"
-import fetch from "node-fetch";
+import 'dotenv/config'
+import fetch from 'node-fetch'
+import { parseArgs } from './helper'
 
-async function upload(text: string) {
-    const url = `${process.env.PUBLISHER}/v1/blobs`
+async function upload(content: string, epochs?: number) {
+	let url = `${process.env.PUBLISHER}/v1/blobs`
 
-    const response = await fetch(url, {
-        method: "PUT",
-        body: text,
-    })
-    const data: any = await response.json()
-    const blobId = data["newlyCreated"]["blobObject"]["blobId"]
-    return `
-    Your content has been uploaded on Walrus!
-    The blob id is: ${blobId}
-    You can view it here: ${process.env.AGGREGATOR}/v1/blobs/${blobId}
-    `
+	if (epochs !== undefined) {
+		url = `${url}?epochs=${epochs}`
+	}
+
+	const response = await fetch(url, {
+		method: 'PUT',
+		body: content,
+	})
+
+	if (!response.ok) {
+		const errorText = await response.text()
+		throw new Error(`Upload failed with status ${response.status}: ${errorText}`)
+	}
+
+	const data: any = await response.json()
+	const blobId = data['newlyCreated']['blobObject']['blobId']
+
+	return `
+            Your content has been uploaded on Walrus!
+            The blob id is: ${blobId}
+            Walrus Scan: https://walruscan.com/testnet/blob/${blobId}
+            You can view it here: ${process.env.AGGREGATOR}/v1/blobs/${blobId}
+            `
 }
 
-const args = process.argv.slice(2)
-upload(args[0]).then(console.log).catch(console.error)
+const rawArgs = process.argv.slice(2)
+const { content, epochs } = parseArgs(rawArgs)
+
+upload(content, epochs).then(console.log).catch(console.error)
